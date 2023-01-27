cfa54c5e742d2c62fb9d663bdbcb8711373dcf2e
Henry Koiti Arasaki <henry_arasaki@hotmail.com>
2023-01-27 00:41:57 -0300

    db 2

--- src/controllers/create-task.ts
+++ src/controllers/create-task.ts
@@ -1,5 +1,6 @@
 
 import { NextFunction,Request,Response } from "express";
+import { DataSource } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Task } from "../entities/task";
 
@@ -15,23 +16,33 @@ export async function createTask(req:Request, res:Response, next:NextFunction){
             throw "No data avalible, cannot save task"
         } 
 
-        const task = await AppDataSource.manager.transaction("REPEATABLE READ",async(transactionalEntityManager)=>{
+        // const task = await AppDataSource.manager.transaction("REPEATABLE READ",async(transactionalEntityManager)=>{
 
-            const repository = transactionalEntityManager.getRepository(Task)
+        //     const repository = transactionalEntityManager.getRepository(Task)
 
-            const task = new Task()
+        //     const task = new Task()
+
+        //     task.title = data.title
+        //     task.done = false
+        //     task.user = data.user
+
+
+       
+
+        //     await repository.save(task)
+
+        //     return task
+        // })
+
+        const repository = await AppDataSource.getRepository(Task)
+
+        const task = new Task()
 
             task.title = data.title
             task.done = false
             task.user = data.user
 
-
-       
-
             await repository.save(task)
-
-            return task
-        })
 
         res.status(200).json({task})
 
